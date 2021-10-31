f49988de8d6d5bdb47725a45236919c7b58a525d
ljr47 <ljjr82@gmail.com>
2021-10-30 22:23:55 -0400

    Committing bottom footer

---
+++ main-app/src/components/Bottom-Footer.vue
@@ -0,0 +1,21 @@
+<template>
+    <div id="wrapper">
+        <p>
+            <b>Group 5: Code Hunters</b><br>
+                Luke Rogers - Captain<br>
+                Stephan Gunawardana<br>
+                Nikhil Khandelwal
+        </p>
+    </div>
+</template>
+<script>
+export default {
+};
+</script>
+<style>
+#wrapper {
+  margin: 0;
+  padding: 0px 0px;
+  width: 100%;
+}
+</style>
