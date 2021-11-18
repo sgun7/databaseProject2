bda4a06dd0a05aba434568af8225ddffd5e45f5a
sgun7 <60078854+sgun7@users.noreply.github.com>
2021-11-18 13:57:18 -0500

    Update admin UI

--- main-app/src/views/Admin.vue
+++ main-app/src/views/Admin.vue
@@ -34,6 +34,83 @@
             </div>
   </div>
 </div>
+
+<div class="card">
+  <v-card
+    :loading="loading"
+    class="mx-auto my-12"
+    max-width="374"
+  >
+    <template slot="progress">
+      <v-progress-linear
+        color="deep-purple"
+        height="10"
+        indeterminate
+      ></v-progress-linear>
+    </template>
+
+    <v-img
+      height="250"
+      src="https://cdn.vuetifyjs.com/images/cards/cooking.png"
+    ></v-img>
+
+    <v-card-title>Full Name: {{this.profile.name}}</v-card-title>
+
+    <v-card-text>
+      <v-row
+        align="center"
+        class="mx-0"
+      >
+        <v-rating
+          :value="4.5"
+          color="amber"
+          dense
+          half-increments
+          readonly
+          size="14"
+        ></v-rating>
+
+        <div class="grey--text ms-4">
+          4.5 (413)
+        </div>
+      </v-row>
+
+      <div class="my-4 text-subtitle-1">
+        $ • Italian, Cafe
+      </div>
+
+      <div>Small plates, salads & sandwiches - an intimate setting with 12 indoor seats plus patio seating.</div>
+    </v-card-text>
+
+    <v-divider class="mx-4"></v-divider>
+
+    <v-card-title>Tonight's availability</v-card-title>
+
+    <v-card-text>
+      <v-chip-group
+        active-class="deep-purple accent-4 white--text"
+        column
+      >
+        <v-chip>5:30PM</v-chip>
+
+        <v-chip>7:30PM</v-chip>
+
+        <v-chip>8:00PM</v-chip>
+
+        <v-chip>9:00PM</v-chip>
+      </v-chip-group>
+    </v-card-text>
+
+    <v-card-actions>
+      <v-btn
+        color="deep-purple lighten-2"
+        text
+      >
+        Reserve
+      </v-btn>
+    </v-card-actions>
+  </v-card>
+</div>
  
 </div>
 </template>
@@ -74,6 +151,13 @@ export default ({
     console.log(user);
         
     },
+    updateCard(){
+        const auth = getAuth();
+
+        const user = auth.currentUser;
+
+        this.profile.name = user.displayName;
+    },
     async mainPress()
     {
         const addAdmin = await firebase.functions().httpsCallable('addAdminRole');
@@ -85,6 +169,7 @@ export default ({
   {
       const auth = getAuth();
       const user = auth.currentUser;
+      this.updateCard();
       console.log(user);
   }
 })
