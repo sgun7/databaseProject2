1d55cf44f8728d56c8709d7cb8f2375c7541c657
Luke <ljjr82@gmail.com>
2021-11-26 18:55:41 -0500

    Fixed about page

--- main-app/src/views/About.vue
+++ main-app/src/views/About.vue
@@ -1,42 +1,45 @@
 <template>
   <div>
     <v-container fill-height fluid>
-      <v-row align="center" justify="center">
-        <h1>About Page</h1>
-      </v-row>
-      <v-row align="center" justify="center">
-          Our goal for the Covid Tracker project was to showcase the list of COVID-19 case counts for various countries around the world. A secondary goal was to show the case counts for various cities inside of the countries.
-      </v-row>
-      <v-row align="center" justify="center">
-        <h3 class="text-decoration-underline">
-            <br /> 
-            <br /> 
-            Meet The Team
-          </h3>
-      </v-row>
-      <v-row align="center" justify="center">
-        <v-col>
-          <br/><h5>Luke Rogers</h5>
-          Team Captain <br/>
-          Major: Computer Engineering <br/>
-          Graduation: May 2022 <br/>
-          Favorite Programming Language: C#
-        </v-col>
-        <v-col>
-          <br/><h5>Stephan Gunawardana</h5>
-          Major: Computer Science<br/>
-          Graduation: May 2022 <br/>
-          Favorite Programming Language: C++
-        </v-col>
-      </v-row>
-        <v-row align="center" justify="center">
-        <v-col>
-          <br/> <h5>Nikhil Khandelwal</h5>
-          Major: <br/>
-          Graduation:<br/>
-          Favorite Programming Language: 
-        </v-col>
-      </v-row>
+      <v-card dark>
+        <v-card-text>
+          <h1><b>About Page</b></h1>
+          <h5>Our goal for the Covid Tracker project was to showcase the list of COVID-19 case counts for various countries around the world. A secondary goal was to show the case counts for various cities inside of the countries.</h5>
+        </v-card-text>
+      </v-card>
+    </v-container>
+    <v-container>
+      <v-card dark>
+        <v-card-text>
+          <h3 class="text-decoration-underline"><b>Meet The Team</b></h3>
+
+          <v-card-text>
+            <h4><b>Luke Rogers</b></h4>
+            <h5>Team Captain <br/>
+            Major: Computer Engineering <br/>
+            Graduation: May 2022 <br/>
+            Favorite Programming Language: C#</h5>
+          </v-card-text>
+          
+          <v-card-text>
+            <h4><b>Stephan Gunawardana</b></h4>
+            <h5>Major: Computer Science<br/>
+            Graduation: May 2022 <br/>
+            Favorite Programming Language: C++</h5>
+          </v-card-text>
+        
+          <v-card-text>
+            <h4><b>Nikhil Khandelwal</b></h4>
+            <h5>Major: Computer Science<br/>
+            Graduation: December 2022<br/>
+            Favorite Programming Language: Java</h5>
+          </v-card-text>   
+        </v-card-text>
+
+              
+
+      </v-card>
+      
     </v-container>
   </div>
 </template>
